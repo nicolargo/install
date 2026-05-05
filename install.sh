c66973ffb3fefff9ffa8ed6a7ba4940090cd6928
nicolargo <nicolashennion@gmail.com>
2026-05-05 22:55:45 +0200

    Install Claude + Rtk

---
+++ install.sh
@@ -0,0 +1,7 @@
+# Claude
+curl -fsSL https://claude.ai/install.sh | bash
+
+# rtk
+# https://github.com/rtk-ai/rtk
+curl -fsSL https://raw.githubusercontent.com/rtk-ai/rtk/refs/heads/master/install.sh | sh
+rtk init -g
